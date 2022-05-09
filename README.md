2e715bfcf6f42774770865f5e0bf5bc51f551682
Pr0pHesyer <74277431+Pr0pHesyer@users.noreply.github.com>
2022-05-09 06:55:18 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# RandomDingzhen
-基于HoshinoBot V2，返回随机丁真梗图
+## 随机丁真插件
+
+## 安装
+1. 在`HoshinoBot/hoshino/modules`目录clone本项目 
+   
+   ```
+   https://github.com/Pr0pHesyer/MUG_one_sentence.git
+   ```
+2. 在`HoshinoBot/hoshino/config/__bot__.py`文件的`MODULES_ON`里添加`RandomDingzhen`
+3. 重启hoshino
+
+## 指令
+
+| 指令               | 说明                  |
+| ---------------- | ------------------- |
+| 随机丁真 | 随机获取一张丁真梗图 |
---
+++ RandomDingzhen.py
@@ -0,0 +1,26 @@
+import json
+import os
+import requests
+from typing import Dict
+
+from hoshino import Service
+from hoshino.typing import MessageSegment, CQEvent
+
+sv = Service('随机丁真')
+
+# 常量
+url = 'https://www.yiyandingzhen.top/'
+randomUrl = url + "getpic.php"
+
+#返回消息格式
+def GetRandomDingzhen(dingzhenJSON: dict) -> str:
+    gotPicUrl = url + dingzhenJSON["picpath"]["pic_path"]
+    msg = f'''{MessageSegment.image(gotPicUrl)}'''
+    return msg
+
+@sv.on_fullmatch("随机丁真")
+async def SentRandomDingzhen(bot, ev: CQEvent):
+    # 获取json字典
+    dingzhenJson = requests.get(randomUrl).json()[0]
+    # 发送消息
+    await bot.send(ev, GetRandomDingzhen(dingzhenJson))
